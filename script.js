f49ea3d72e0ac192d35d2894fae0e26d3d7c5da8
hannahrajarao <hannahrajarao@gmail.com>
2023-05-11 01:13:05 -0400

    Add time based color functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const today = dayjs();
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -15,9 +16,26 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  setColors();
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+function setColors() {
+  const currentHour = parseInt(today.format('H'));
+  for(var i=9; i<=17; i++) {
+    var eventId = `#hour-${i}`;
+    if(currentHour < i) {
+      $(eventId).children('textarea').addClass('future');
+    }
+    else if(currentHour === i) {
+      $(eventId).children('textarea').addClass('present');
+    }
+    else {
+      $(eventId).children('textarea').addClass('past');
+    }
+  }
+}
